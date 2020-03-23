49e4082244abb37cbd0c52092f3c30bf6b56fed9
wheredoesyourmindgo <moemanchu@icloud.com>
2020-03-23 10:40:07 -0700

    Fix shell syntax specificity.

--- src/syntax.ts
+++ src/syntax.ts
@@ -277,9 +277,9 @@ export default [
       "variable.other.normal.shell", // Added.
       "variable.other.special.shell", // Added.
       "variable.other.positional.shell", // Added.
-      "variable.other.normal punctuation.definition.variable.shell", // Added.
-      "variable.special.normal punctuation.definition.variable.shell", // Added.
-      "variable.positional.normal punctuation.definition.variable.shell", // Added.
+      "variable.other.normal.shell punctuation.definition.variable.shell", // Added.
+      "variable.special.normal.shell punctuation.definition.variable.shell", // Added.
+      "variable.positional.normal.shell punctuation.definition.variable.shell", // Added.
       "variable.other.bracket.shell", // Added.
       "variable.other.makefile", // Added.
       "meta.tag.other entity.other.attribute-name" // Added.
