c9528cea6daef32804a8238453734103c70f2dc5
wheredoesyourmindgo <moemanchu@icloud.com>
2020-05-05 07:17:01 -0700

    Fix colors standalne.

--- src/standalone/colors.ts
+++ src/standalone/colors.ts
@@ -3,9 +3,10 @@ import {getPalette} from './palette'
 function producePalette(palette) {
   return {
     comment: palette.grey1,
+    comment_emphasis: palette.grey2,
     text: palette.fg,
     keyword: palette.purple,
-    variable: palette.fg0,
+    variable: palette.fg,
     annotation: palette.fg1,
     constant: palette.yellow,
     tag: palette.orange,
@@ -13,8 +14,7 @@ function producePalette(palette) {
     stringInterpolated: palette.green,
     number: palette.red,
     function: palette.aqua,
-    // support: palette.yellow,
-    support: palette.fg0,
+    support: palette.aqua,
     misc: palette.blue,
     invalid: palette.red
   }
